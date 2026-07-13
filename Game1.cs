eb0fedf58779a8c42b12e2b685878d49904df517
dainedwards <dainedwards@gmail.com>
2026-07-13 02:18:29 -0700

    chore: auto-commit from Claude Code

--- Game1.cs
+++ Game1.cs
@@ -385,7 +385,8 @@ public sealed partial class DwarfMinerGame : Game
             var kinds = new[] { CreatureKind.SporeBat, CreatureKind.CrystalCrawler, CreatureKind.VoidWraith,
                                 CreatureKind.CaveSlime, CreatureKind.AcidSpitter, CreatureKind.BomberBeetle,
                                 CreatureKind.SnapperVine, CreatureKind.RockMimic,
-                                CreatureKind.Civilian, CreatureKind.Lizardman };
+                                CreatureKind.Civilian, CreatureKind.Lizardman,
+                                CreatureKind.Peacekeeper };
             for (var i = 0; i < kinds.Length; i++)
                 _run.Creatures.Add(new Creature(
                     _run.Player.Position + fRight * (26f + i * 22f) + fUp * 8f, kinds[i]));
