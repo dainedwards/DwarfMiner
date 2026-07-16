8309f3a3f792300796301dcf09faf6595e698e71
dainedwards <dainedwards@gmail.com>
2026-07-16 08:07:56 -0700

    chore: auto-commit from Claude Code

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Runtime.InteropServices;
 using DwarfMiner.Entities;
 using DwarfMiner.Rendering;
 using DwarfMiner.Space;
